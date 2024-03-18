d030b17753d70e58caf1334f14f5a540057ed047
Ky1o <122052276+Ky1o@users.noreply.github.com>
2024-03-18 19:18:20 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,5 @@
-RegisterNetEvent("Kick-System:kickPlayer")
-AddEventHandler("Kick-System:kickPlayer", function(target, reason)
+
+function kickPlayer(target, reason)
     local targetPlayer = tonumber(target)
 
     if targetPlayer == source then
@@ -11,5 +11,25 @@ AddEventHandler("Kick-System:kickPlayer", function(target, reason)
         else
             lib.notify(source, {title = 'Player is not online.', type = 'error'})
         end
+    end
+end
+
+RegisterCommand("kick",function()
+    local input = lib.inputDialog(source, 'Kick', {'ID', 'Reason'})
+    Citizen.Wait(1500)
+    local alert = lib.alertDialog(source, {
+        header = '# Alert #',
+        content = 'Are you sure you want to kick a player with the ID: ' .. input[1] .. ' ?',
+        centered = true,
+        cancel = true,
+        labels = {
+            cancel = "No",
+            confirm = "Yes"
+        }
+    })
+    if alert == "confirm" then
+        kickPlayer(input[1], input[2])
+    elseif alert == "cancel" then
+        lib.notify({title = 'Canceled.', type = 'error'})
     end
 end)
